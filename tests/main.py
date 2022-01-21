5783e9444547ece15b978362aa1e7ecfa6b4d34e
Noah Altunian <nbaltunian@gmail.com>
2022-01-21 13:25:22 -0500

    refactor: Import gettempdir from tempfile package

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,11 @@
 import os
 import sys
-import tempfile
 import unittest
 
 from pathlib import Path
 from shutil import rmtree
 from subprocess import DEVNULL, STDOUT, check_call
+from tempfile import gettempdir
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -14,7 +14,7 @@ from ugit.data import GIT_DIR
 
 class UgitCore(unittest.TestCase):
     def setUp(self):
-        os.chdir(tempfile.gettempdir())
+        os.chdir(gettempdir())
         if Path(GIT_DIR).is_dir():
             rmtree(GIT_DIR)
 
